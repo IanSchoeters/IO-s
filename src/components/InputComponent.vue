4b85abd3d78b956683140c8b324caf51d53fc3ce
Ian Schoeters <ian.schoeters@gmail.com>
2021-11-14 19:23:38 +0100

    m

--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>input comp works, baby</div>
+  <form action="">
+    <input type="text" placeholder="inkooporder" />
+    <input type="text" placeholder="lijnnr" />
+    <input type="text" placeholder="artikel" />
+    <input type="text" placeholder="naam" />
+    <input type="date" />
+    <span>timestamp today</span>
+  </form>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'InputComponent',
-
-})
+});
 </script>
 
+<style lang="scss" scoped>
+form {
+  margin-top: 3em;
+}
+input {
+  margin-right: 1em;
+}
+</style>
--- src/pages/Deleted.vue
+++ src/pages/Deleted.vue
@@ -1,11 +1,11 @@
 <template>
-    <div>Deleted page works</div>
+  <div>Deleted page works</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
- 
-})
+  name: 'DeletedPage',
+});
 </script>
