e1285274c77b86647319a2288c3af2d18e60f294
Ian Schoeters <ian.schoeters@gmail.com>
2021-11-14 19:23:14 +0100

    m

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,11 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/Index.vue') }],
+    children: [
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: 'deleted', component: () => import('pages/Deleted.vue') },
+      { path: 'all', component: () => import('pages/All.vue') },
+    ],
   },
 
   // Always leave this as last one,
